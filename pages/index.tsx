23cb310bb45223e8e4c21cf7cc89313ab7efb375
Andy Braga <bragovo@gmail.com>
2022-10-03 19:20:03 +0300

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>The FLux</title>
+        <link rel="icon" href="data:;base64,iVBORw0KGgo=" />
+      </Head>
+
+
+      <div>THE FLUX</div>
+    </div>
+  )
+}
+
+export default Home
